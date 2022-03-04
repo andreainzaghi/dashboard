1257ea25b1b3b6b908eced5810ea4204042fa2a6
Andrea <andrea3aprile99@gmail.com>
2022-03-04 18:31:03 +0100

    h

--- pages/index.vue
+++ pages/index.vue
@@ -73,14 +73,54 @@
       <div>
         <h1>Ciao, Ciro Tartaglia</h1>
       </div>
+      <div style="display:flex;justify-content:space-between;align-items:center;" id="dashchoose">
+        <div id="ddffgg">
+          <div><h3>Conto Virtuale</h3></div>
+          <div>0.00$</div>
+          <div><p>Disponibile</p></div>
+        </div>
+
+         <div id="ddffgg">
+          <div><h3>Investimenti</h3></div>
+          <div>0.00$</div>
+          <div><p>Investi</p></div>
+        </div>
+
+         <div id="ddffgg">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="50px" height="50px" viewBox="0 0 50 50" version="1.1" class="svg card-title replaced-svg">
+                  <!-- Generator: Sketch 55.2 (78181) - https://sketchapp.com -->
+                  <title>icon-cash-out</title>
+                  <desc>Created with Sketch.</desc>
+                  <g id="icon-cash-out" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                      <g id="download-archive-tray" fill="#000000" fill-rule="nonzero">
+                          <g id="add-button-inside-black-circle" transform="translate(5.000000, 5.000000)">
+                              <path d="M20,0 C31,0 40,9 40,20 C40,31 31,40 20,40 C9,40 0,31 0,20 C0,9 9,0 20,0 Z M20.0457456,26.499693 L28.0457456,18.3178895 L23.2457561,18.3178895 L23.2457561,8.49969298 L16.8457351,8.49969298 L16.8745251,18.3178895 L12.0457456,18.3178895 L20.0457456,26.499693 Z M26.7124123,26.499693 L26.7124123,29.499693 L13.3790789,29.499693 L13.3790789,26.499693 L10.0457456,26.499693 L10.0457456,29.499693 L10.0457456,32.499693 L30.0457456,32.499693 L30.0457456,26.499693 L26.7124123,26.499693 Z" id="Shape"></path>
+                          </g>
+                          <path d="M25.0033784,0 C11.1991892,0 0.00716216216,11.1924324 0.00716216216,24.997027 C0.00716216216,38.8006757 11.1991892,49.9932432 25.0033784,49.9932432 C38.8078378,49.9932432 49.9995946,38.8008108 49.9995946,24.997027 C49.9995946,11.1924324 38.8078378,0 25.0033784,0 Z M25.0033784,47.7722973 C12.4436486,47.7722973 2.22824324,37.5543243 2.22824324,24.997027 C2.22824324,12.4389189 12.4436486,2.22175676 25.0033784,2.22175676 C37.5616216,2.22175676 47.7777027,12.4387838 47.7777027,24.997027 C47.7777027,37.5541892 37.5614865,47.7722973 25.0033784,47.7722973 Z" id="Shape"></path>
+                      </g>
+                  </g>
+              </svg>
+          </div>
+          <div><h4>Cash-Out</h4></div>
+          <div><p>Credito</p></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <style scoped>
+#ddffgg{
+  padding:20px;
+  border:1px solid black;
+  height:200px;
+  width:30%;
+  
+}
 #containerprincipe{
   /* height:650px; */
   /* height:150px; */
-  border:1px solid black;width:100%;position:relative;background-color:#8EDCF4;padding:20px 30px;
+  border:1px solid black;width:100%;position:relative;background-color:#8EDCF4;padding:20px 30px;border-radius:10px;
 }
 .payment
 {
