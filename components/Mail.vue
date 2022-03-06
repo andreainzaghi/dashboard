15971df27da0e131d6da1e58582365808518d83e
Andrea <andrea3aprile99@gmail.com>
2022-03-06 18:51:55 +0100

    mail-sendmessage

--- components/Mail.vue
+++ components/Mail.vue
@@ -2,17 +2,31 @@
     <div>
         <div v-if="openform == true" style="width:300px;height:500px;border:1px solid lightgrey;background-color:#fff;border-radius:10px;padding:15px;position:fixed;bottom:100px;right:40px;z-index:22;">
             <div id="exitid" @click="OpenMail1()" style="position:absolute;top:15px;right:15px;border-radius:20px;padding:1px 7px;border:1px solid grey;">EXIT</div>
-            <div>
+            <div >
                 <img src="img/logo-recrowd.svg" alt="" style="height:40px;">
                 <div>
                    <h3>Assistenza</h3> 
                    <h5>Offline icon Offline</h5>
                 </div>
                 <div style="width:100%;border:1px solid lightgrey;"> </div>
+        
+                <div style="overflow:scroll;width:100%;max-height:325px;padding:1px;">
+                  <div id="backmessage"  v-for="element in messge"  :key="element.id" >
+                    <div v-if="element.status == 'sent'">
+                        <p >{{element.message}}</p>
+                        <p><small>{{element.date}}</small></p>
+                    </div>
+                      <!-- <div class="verde1" v-else>
+                        <p>{{element.message}}</p>
+                        <p><small>{{element.date}}</small></p>
+                    </div>      -->
+                </div> 
+              </div>
                  <div style="width:calc(100% - 30px);border:1px solid lightgrey;margin-top:10px;position:absolute;bottom:15px;right:15px;left:15px;padding:10px;background-color:lightgrey;">
+                  
                      <div style="display:flex;align-items:center;">
-                         <input type="text" style="background-color:#fff;width:100%; border-bottom-left-radius:50px; border-top-left-radius:50px;font-size:12px;padding:10px;border:none;outline:none;border:1px solid grey;" placeholder="In che modo possiamo esserti utili?">
-                         <div style="cursor:pointer; border-bottom-right-radius:50%; background-color:#22AB6E;color:#fff; border-top-right-radius:50%;font-size:12px;padding:10px;border:none;outline:none;border-right:1px solid grey;border-top:1px solid grey;border-bottom:1px solid grey;">SEND</div>
+                         <input v-model="messagemail"  type="text" style="background-color:#fff;width:100%; border-bottom-left-radius:50px; border-top-left-radius:50px;font-size:12px;padding:10px;border:none;outline:none;border:1px solid grey;" placeholder="In che modo possiamo esserti utili?">
+                         <div @click="SendMessage()" style="cursor:pointer; border-bottom-right-radius:50%; background-color:#22AB6E;color:#fff; border-top-right-radius:50%;font-size:12px;padding:10px;border:none;outline:none;border-right:1px solid grey;border-top:1px solid grey;border-bottom:1px solid grey;">SEND</div>
                      </div>
                  </div>
             </div>
@@ -38,10 +52,34 @@ export default {
   data () {
     return {
       openform: false,
-      finish:false      
+      finish:false   ,
+      sendmess:false   ,
+      messaggiovar:null,
+       currentUser:0,
+     
+         messge:[ {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                },
+          ]
     }
   },
    methods:{
+     SendMessage(){
+        // this.sendmess=true;
+          // this.messaggiovar= this.messagemail
+          // this.messagemail=''
+           if (this.messagemail !== ''){
+       this.messge.push({
+            date: 'DD/MM/YY HH.MM',
+            message: this.messagemail,
+            status: 'sent'
+            });
+         this.messagemail = '';
+
+    }
+     },
      OpenMail() {
        this.openform=true;
         this.first=true;
@@ -60,6 +98,10 @@ export default {
 </script>
 
 <style scoped>
+#backmessage{
+  padding:10px;margin-top:5px;width:75%;border-radius:10px;font-size:12.5px;
+   animation: myfirst2 9s linear 0.1s infinite alternate; font-weight:bold;color:black;
+}
 #mail{
     height:60px;
     width:60px;
