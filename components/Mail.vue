f2234d865282071388da7cdbe824b8340c5dfbc2
Andrea <andrea3aprile99@gmail.com>
2022-03-06 19:19:29 +0100

    mail-send

--- components/Mail.vue
+++ components/Mail.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div v-if="openform == true" style="width:300px;height:500px;border:1px solid lightgrey;background-color:#fff;border-radius:10px;padding:15px;position:fixed;bottom:100px;right:40px;z-index:22;">
+        <div v-if="openform == true" style="width:340px;height:500px;border:1px solid lightgrey;background-color:#fff;border-radius:10px;padding:15px;position:fixed;bottom:100px;right:40px;z-index:22;">
             <div id="exitid" @click="OpenMail1()" style="position:absolute;top:15px;right:15px;border-radius:20px;padding:1px 7px;border:1px solid grey;">EXIT</div>
             <div >
                 <img src="img/logo-recrowd.svg" alt="" style="height:40px;">
