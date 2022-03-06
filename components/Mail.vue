00e48d51c1767a2d71603bb2c2bc3f2088cc9dc1
Andrea <andrea3aprile99@gmail.com>
2022-03-06 15:19:44 +0100

    d

--- components/Mail.vue
+++ components/Mail.vue
@@ -1,13 +1,34 @@
 <template>
     <div>
+        <div v-if="openform == true" style="width:300px;height:500px;border:1px solid lightgrey;background-color:#fff;border-radius:10px;padding:15px;position:fixed;bottom:120px;right:40px;z-index:22;">
+            <div id="exitid" @click="OpenMail1()" style="position:absolute;top:15px;right:15px;border-radius:20px;padding:3px 10px;border:1px solid grey;">EXIT</div>
+            <div>
+                <img src="img/logo-recrowd.svg" alt="" style="height:40px;">
+                <div>
+                   <h3>Assistenza</h3> 
+                   <h5>Offline icon Offline</h5>
+                </div>
+                <div style="width:100%;border:1px solid lightgrey;"> </div>
+                 <div style="width:calc(100% - 30px);border:1px solid lightgrey;margin-top:10px;position:absolute;bottom:15px;right:15px;left:15px;padding:10px;background-color:lightgrey;">
+                     <div>
+                         <input type="text" style="background-color:#fff;width:100%;border-radius:20px;font-size:12px;padding:10px;border:none;outline:none;border:1px solid grey;" placeholder="In che modo possiamo esserti utili?">
+                     </div>
+                 </div>
+            </div>
+        </div>
         <div id="mail" @click="OpenMail()">
                     <img src="img/mails.png" alt="" style="width:60px;">
         </div>
-        <!-- <div style="width:300px;height:500px;backgroud-color:red;position:fixed;bottom:120px;z-index:22;">
-<div>s</div>
-        </div> -->
+        
     </div>
 </template>
+<style scoped>
+#exitid:hover{
+    background:lightgrey;
+
+}
+
+</style>
 
 <script>
 export default {
@@ -22,8 +43,13 @@ export default {
      OpenMail() {
        this.openform=true;
         this.first=true;
-        const linea1 = document.querySelector('#containerprincipe')
-          linea1.style.height='450px';
+     
+               
+     },
+     OpenMail1() {
+       this.openform=false;
+        this.first=false;
+     
                
      },
  
